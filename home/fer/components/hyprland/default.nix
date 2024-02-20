541077a17924e6181610b4a03b225f295a0eb512
Fer L. <afriguez@gmail.com>
2024-02-20 14:17:29 -0500

    feat: add packages and enable fontconfig in hyprland

--- home/fer/components/hyprland/default.nix
+++ home/fer/components/hyprland/default.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }: {
+  fonts.fontconfig.enable = true;
+
   home.packages = with pkgs; [
     grim
     slurp
     wl-clipboard
+    discord
+    noto-fonts-cjk
+    swww
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 }
