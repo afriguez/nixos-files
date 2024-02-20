2061d0e6a13465e4da18f0316d31aae4b965cf65
Fer L. <afriguez@gmail.com>
2024-02-20 13:17:30 -0500

    feat: add home-manager to flake

--- flake.nix
+++ flake.nix
@@ -4,18 +4,36 @@
   outputs =
     inputs@{ self
     , nixpkgs
+    , home-manager
     , ...
     }:
+    let
+      inherit (self) outputs;
+      lib = nixpkgs.lib // home-manager.lib;
+    in
     {
+	  inherit lib;
       nixosConfigurations = {
-        afriguez = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./nixos/configuration.nix
-          ];
+        marija = lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/marija ];
+        };
+      };
+
+      homeConfigurations = {
+        "fer@marija" = lib.homeManagerConfiguration {
+          modules = [ ./home/fer/marija.nix ];
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
         };
       };
     };
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
