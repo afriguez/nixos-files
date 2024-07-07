43d6a510a0264a21a0783710d692709b950dfef4
Fer L. <afriguez@gmail.com>
2024-07-06 20:41:01 -0500

    feat(omaru): enable TCPIP(psql) and auth local network

--- hosts/omaru/default.nix
+++ hosts/omaru/default.nix
@@ -44,11 +44,13 @@
     };
     postgresql = {
       enable = true;
+      enableTCPIP = true;
       authentication = pkgs.lib.mkOverride 10 ''
         #type database DBuser auth-method
         local all      all    trust
         host  all all 127.0.0.1/32 trust
         host  all all ::1/128      trust
+        host  all all 192.168.0.1/24 trust
       '';
     };
   };
