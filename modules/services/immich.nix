ec75f3eb85a174e428285c44224622bd99c37d2a
Fer L. <afriguez@gmail.com>
2026-09-02 10:27:06 -0500

    feat(buro): update immich config

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -5,6 +5,11 @@
       enable = true;
       host = "0.0.0.0";
       openFirewall = true;
+      mediaLocation = "/home/fer/external/server/immich";
     };
+  };
+
+  systemd.services.immich-server.unitConfig = {
+    ConditionPathIsMountPoint = "/home/fer/external";
   };
 }
